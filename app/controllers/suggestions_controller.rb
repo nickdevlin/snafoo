475d8cd0c7cbe64eed5f6843d555b787f2491aed
Nick Devlin <nrdevlin@gmail.com>
2017-04-07 19:01:10 -0500

    Suggested snacks added to API and homepage

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -3,8 +3,20 @@ require 'net/http'
 class SuggestionsController < ApplicationController
   include SnacksHelper
 
-  def show
+  def index
     classic_snacks
+  end
+
+  def new
+    classic_snacks
+  end
+
+  def create
+    new_snack_name = params[:new_snack]["Name"]
+    new_snack_location = params[:new_snack]["Location"]
+    add_new_snack(new_snack_name, new_snack_location)
+    classic_snacks
+    redirect_to root_path
   end
 
 end
--- app/helpers/snacks_helper.rb
+++ app/helpers/snacks_helper.rb
@@ -1,4 +1,4 @@
-require 'net/http'
+require 'rest-client'
 require 'json'
 
 module SnacksHelper
@@ -6,7 +6,13 @@ module SnacksHelper
 
   #retrieve snacks that are always ordered
   def classic_snacks
-    uri = URI(SNACKS_URL)
-    @snacks = JSON.parse(Net::HTTP.get(uri))
+    all_snacks = RestClient.get SNACKS_URL
+    @snacks = JSON.parse(all_snacks)
+  end
+
+  #add new snack to data feed
+  def add_new_snack(snack_name, snack_location)
+    new_snack = RestClient.post SNACKS_URL, {name: snack_name, location: snack_location}.to_json, content_type: :json
+    added_snack = JSON.parse(new_snack)
   end
 end
