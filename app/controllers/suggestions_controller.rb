49b3aae14acbc5deedebce2accf45d8981c95b91
Nick Devlin <nrdevlin@gmail.com>
2017-04-09 19:41:00 -0500

    Separate methods in snack helper

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -4,18 +4,17 @@ class SuggestionsController < ApplicationController
   include SnacksHelper
 
   def index
-    classic_snacks
+    retrieve_all_snacks
   end
 
   def new
-    classic_snacks
+    retrieve_suggested_snacks
   end
 
   def create
     new_snack_name = params[:new_snack]["Name"]
     new_snack_location = params[:new_snack]["Location"]
     add_new_snack(new_snack_name, new_snack_location)
-    classic_snacks
     redirect_to root_path
   end
 
--- app/helpers/snacks_helper.rb
+++ app/helpers/snacks_helper.rb
@@ -4,10 +4,25 @@ require 'json'
 module SnacksHelper
   SNACKS_URL = 'https://api-snacks.nerderylabs.com/v1/snacks?ApiKey=7a29a6d3-7d09-47d9-af3d-072ea9159080'
 
-  #retrieve snacks that are always ordered
-  def classic_snacks
-    all_snacks = RestClient.get SNACKS_URL
-    @snacks = JSON.parse(all_snacks)
+  #parse that feed into something we can use
+  def parse_all_snacks
+    JSON.parse(RestClient.get SNACKS_URL)
+  end
+
+  #filter out snacks that are always ordered
+  def retrieve_classic_snacks
+    @classic_snacks = parse_all_snacks.select { |snack| snack["optional"] == false }
+  end
+
+  #filter snacks under consideration
+  def retrieve_suggested_snacks
+    @suggested_snacks = parse_all_snacks.select { |snack| snack["optional"] == true }
+  end
+
+  #combine methods into one for use in suggestions controller
+  def retrieve_all_snacks
+    retrieve_suggested_snacks
+    retrieve_classic_snacks
   end
 
   #add new snack to data feed
@@ -15,4 +30,10 @@ module SnacksHelper
     new_snack = RestClient.post SNACKS_URL, {name: snack_name, location: snack_location}.to_json, content_type: :json
     added_snack = JSON.parse(new_snack)
   end
+
+
+
+
+
+
 end
