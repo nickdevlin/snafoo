ec9f1a4121c27b093b9ca72695ef2a914ad75f38
Nick Devlin <nrdevlin@gmail.com>
2017-04-10 00:24:53 -0500

    Make voting functional with some error handling

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,10 +1,9 @@
-require 'net/http'
-
 class SuggestionsController < ApplicationController
   include SnacksHelper
 
   def index
     retrieve_all_snacks
+    @votes = Vote.all
   end
 
   def new
@@ -12,9 +11,7 @@ class SuggestionsController < ApplicationController
   end
 
   def create
-    new_snack_name = params[:new_snack]["Name"]
-    new_snack_location = params[:new_snack]["Location"]
-    add_new_snack(new_snack_name, new_snack_location)
+    add_new_snack(params[:new_snack]["Name"], params[:new_snack]["Location"])
     redirect_to root_path
   end
 
